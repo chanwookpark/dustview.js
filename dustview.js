cb1b57c2677de696e513ad42206f0f37993031ba
chanwook <chanwook.god@gmail.com>
2014-03-19 08:29:02 +0900

    dustview 내부 코드 리팩터링
    함수로 구분

--- dustview.js
+++ dustview.js
@@ -2,15 +2,28 @@ var dust = require("dustjs-linkedin");
 var fs = require('fs');
 
 function render(_templateKey, _source, _model, _res) {
+	
+	var compiledSource = dcompile(_templateKey, _source);
+
+	dloadSource(compiledSource);
+
+	drender(_templateKey, _model, _res);
+}
+
+function dcompile(_templateKey, _source) {
 	var tmeplate = new String(_source);
 	//TODO 상대경로는 어떻게 찾아낼 것인가? 
 	if(_source.indexOf('.') === 0 || _source.indexOf('/') === 0) {
 		tmeplate = fs.readFileSync(_source).toString();		
 	}
-	var compiledSource = dust.compile(tmeplate, _templateKey);
+	return dust.compile(tmeplate, _templateKey);
+}
 
-	dust.loadSource(compiledSource);
+function dloadSource(_compiledSource) {
+	dust.loadSource(_compiledSource);
+}
 
+function drender(_templateKey, _model, _res) {
 	var json = "";
 	if(_model instanceof String) {
 		json = JSON.parse(_model);
@@ -32,3 +45,6 @@ function render(_templateKey, _source, _model, _res) {
 }
 
 exports.render = render;
+exports.dcompile = dcompile;
+exports.dloadSource = dloadSource;
+exports.drender = drender;
